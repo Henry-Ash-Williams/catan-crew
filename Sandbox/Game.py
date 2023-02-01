fdfdb45b79c90eb2993339bb606cd8c5aeca585c
Guy <ga327@sussex.ac.uk>
2023-02-01 02:32:32 +0000

    Cleaned up code

--- Sandbox/Game.py
+++ Sandbox/Game.py
@@ -3,15 +3,16 @@ class Game:
   def __init__(game, board, players):
     game.board = board
     board.game = game
+    game.turn_count = 0
     
     requested_colors = set(p.color for p in players)
-    if len(requested_colors) == len(players):
-      game.players = players
-      game.player_number = len(game.players)
-      for player in game.players:
-        player.game = game
-    else:
+    if len(requested_colors) != len(players):
       raise Exception('More than one player have the same color')
+    
+    game.players = players
+    game.player_number = len(game.players)
+    for player in game.players:
+      player.game = game
     
     game.current_player_index = 0
     game.current_player = game.players[game.current_player_index]
@@ -22,3 +23,4 @@ class Game:
     
     game.current_player_index = (game.current_player_index + 1)% game.player_number
     game.current_player = game.players[game.current_player_index]
+    game.turn_count += 1
