e1e197bb4a1ebcbd2f3d58b4370b828a46598d1e
Henry-Ash-Williams <henrywilliams95000@gmail.com>
2023-02-06 13:08:02 +0000

    Added gitignore, class outline, and worked on e2e testing

--- Sandbox/README.txt
+++ Sandbox/README.txt
@@ -2,6 +2,7 @@ How to test this code:
 
 1. Download contents of folder
 2. Open command prompt / bash shell inside folder
+3. run the following command `pip install rich`
 3. Type "python test.py"
 4. If you don't get any errors, that means it works.
 
--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,36 +1,33 @@
 from Board import *
 from Player import *
 from Game import *
+from rich import print
 
-newBoard = Board(size=3)
+def main():
+    newBoard = Board(size=3)
 
-alice = Player('blue')
-bob = Player('red')
-charlie = Player('green')
-david = Player('purple')
+    alice = Player('blue')
+    bob = Player('red')
+    charlie = Player('green')
+    david = Player('purple')
 
-game = Game(board=newBoard, players=[alice, bob, charlie, david])
+    player_order = [alice, bob, charlie, david]
+    colors = [player.color for player in player_order]
+    print(f"player order: {[f'[bold {color}]{color}[/bold {color}]' for color in colors]}")
 
-# Setup phase 1
-alice.builds_settlement(location=419)
-alice.builds_road(location=420)
-alice.ends_turn()
+    game = Game(board=newBoard, players=player_order)
 
-bob.builds_settlement(location=503)
-bob.builds_road(location=480)
-bob.ends_turn()
+    game.begin_setup_phase()
+    for player in player_order + reversed(player_order):
+        player.builds_settlement(location=0)
+        player.builds_road(location=0)
+        player.ends_turn()
+    game.distribute_resources()
+    game.end_setup_phase()
 
-charlie.builds_settlement(location=415)
-charlie.builds_road(location=393)
-charlie.ends_turn()
+if __name__ == "__main__":
+    main()
 
-david.builds_settlement(location=88)
-david.builds_road(location=111)
-
-# Setup phase 2
-david.builds_settlement(location=94)
-david.builds_road(location=117)
-david.ends_turn()
 
 # alice.roll_dice()
 # alice.get_resources(...)
