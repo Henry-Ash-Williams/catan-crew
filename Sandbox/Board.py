f4354a8a47ee4fc67630a5d0ceab529f30c629ee
Guy <ga327@sussex.ac.uk>
2023-02-08 01:32:15 +0000

    Added unit tests for Board

--- Sandbox/Board.py
+++ Sandbox/Board.py
@@ -80,41 +80,41 @@ class Board:
         #  (X + cardinal_directions[3]*2)%board.cell_count would be the index
         #  of the cell 2 steps away in the southwest direction from cell X
 
-        board.cells = [None] * board.cell_count
-        board.harbors = board.select(0, size, dir_pattern=(2, 2))
-        board.bridges = join(board.select(h, 1) for h in board.harbors)
-        board.tiles = join(
-            board.select(0, i, dir_pattern=(2, 2)) for i in range(n)) + [0]
-        board.intersections = join(
-            board.select(t, 1, (2, )) for t in board.tiles)
-        board.paths = join(board.select(t, 1, (1, 1)) for t in board.tiles)
+        board.cells         = [None] * board.cell_count
+        board.harbors       = board.select(0, size, dir_pattern=(2, 2))
+        board.bridges       = join(board.select(h, 1) for h in board.harbors)
+        board.tiles         = join(board.select(0, i, dir_pattern=(2, 2)) for i in range(n)) + [0]
+        board.intersections = join(board.select(t, 1, (2, )) for t in board.tiles)
+        board.paths         = join(board.select(t, 1, (1, 1)) for t in board.tiles)
 
         for location in board.intersections:
             board.cells[location] = Intersection()
+            
         for location in board.paths:
             board.cells[location] = Path(location)
 
         board.desert_tiles = [0]
         board.resource_number = len(board.tiles) - len(board.desert_tiles)
-        resources = ['grain'] * 4 + ['wool'] * 4 + ['lumber'] * 4 + [
-            'brick'
-        ] * 3 + ['ore'] * 3
-        number_tokens = [
-            5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11
-        ]
+        resources = ['grain'] * 4 + ['wool'] * 4 + ['lumber'] * 4 + \
+                    ['brick'] * 3 + ['ore'] * 3
+        number_tokens = [5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11]
+        
         board.resources = (resources * (
             (board.resource_number + 17) // 18))[:board.resource_number]
+            
         board.number_tokens = (number_tokens * (
             (board.resource_number + 17) // 18))[:board.resource_number]
+            
         random.shuffle(board.resources)
         random.shuffle(board.number_tokens)
+        
         del resources, number_tokens
 
         for location in board.tiles:
-            resource = 'nothing' if (
-                location in board.desert_tiles) else board.resources.pop()
-            number_token = None if (
-                location in board.desert_tiles) else board.number_tokens.pop()
+            resource = 'nothing' if (location in board.desert_tiles) \
+                                 else board.resources.pop()
+            number_token = None if (location in board.desert_tiles) \
+                                else board.number_tokens.pop()
             tile = Tile(location, resource, number_token)
             board.cells[location] = tile
 
@@ -176,7 +176,7 @@ class Board:
                     board.cells[location].build_road(road)
                     return
 
-        raise Exception("Player can't reach given path")
+        raise PathBuildingException("Player can't reach given path")
 
     def add_settlement(board, location, settlement,
                        allow_disconnected_settlement):
@@ -249,3 +249,6 @@ class Board:
         with open(filename, "rb") as file:
             pickle = Unpickler(file)
             return pickle.load()
+
+
+class PathBuildingException(Exception): pass
--- Sandbox/Board_test.py
+++ Sandbox/Board_test.py
@@ -1,18 +1,75 @@
 #!/usr/bin/env python3
 
 from Board import *
-import unittest
+from Player import *
+import unittest, pickle, random
 
 
 class BoardTester(unittest.TestCase):
-    # Read the serialized board from disk and store it in a member variable,
-    # `self.board` will always be available for use throughout the testing
-    # suite
-    def setUp(self):
-        self.board = Board.read_state("board.pickle")
 
-    def test_init(self):
-        print(self.board)
+    def setUp(test):
+      test.board = Board(seed=random.random())
+      try:
+        with open('BoardTestResults.pickle','rb+') as f:
+          test.past_results = pickle.load(f)
+      except FileNotFoundError:
+        test.past_results = {'resource_distributions':[], 'token_distributions': []}
+
+    def test_init(test):
+      test.assertTrue(type(test.board)==Board)
+    
+    def test_harbor_count(test):
+      test.assertEqual(len(test.board.harbors),18)
+    
+    def test_count_count(test):
+      test.assertEqual(len(test.board.bridges),108)
+    
+    def test_tile_count(test):
+      test.assertEqual(len(test.board.tiles),19)
+    
+    def test_intersection_count(test):
+      test.assertEqual(len(test.board.intersections),54)
+    
+    def test_path_count(test):
+      test.assertEqual(len(test.board.paths),72)
+      
+    def test_resource_randomness(test):
+      tiles = [test.board.cells[location] for location in test.board.tiles]
+      resource_distribution = tuple([tile.resource for tile in tiles])
+      test.assertFalse(resource_distribution in test.past_results['resource_distributions'])
+      test.past_results['resource_distributions'].append(resource_distribution)
+      
+    def test_resource_type_count(test):
+      tiles = [test.board.cells[location] for location in test.board.tiles]
+      resources = tuple([tile.resource for tile in tiles])
+      test.assertEqual(resources.count('grain'),4)
+      test.assertEqual(resources.count('wool'),4)
+      test.assertEqual(resources.count('lumber'),4)
+      test.assertEqual(resources.count('brick'),3)
+      test.assertEqual(resources.count('ore'),3)
+      test.assertEqual(resources.count('nothing'),1)
+      
+    def test_number_token_randomness(test):
+      tiles = [test.board.cells[location] for location in test.board.tiles]
+      token_distribution = tuple([tile.number_token for tile in tiles])
+      test.assertFalse(token_distribution in test.past_results['token_distributions'])
+      test.past_results['token_distributions'].append(token_distribution)
+      
+    def test_token_count(test):
+      tiles = [test.board.cells[location] for location in test.board.tiles]
+      tokens = tuple([tile.number_token for tile in tiles])
+      test.assertEqual(set(tokens), set([None,2,3,4,5,6,8,9,10,11,12]))
+      
+    def test_add_road(test):
+      test_player = Player('blue')
+      for path_location in test.board.paths:
+        test_road = Road(path_location, test_player)
+        with test.assertRaises(PathBuildingException) as e:
+          test.board.add_road(path_location, test_road)
+      
+    def tearDown(test):
+      with open('BoardTestResults.pickle','wb+') as f:
+        pickle.dump(test.past_results, f)
 
 
 if __name__ == "__main__":
