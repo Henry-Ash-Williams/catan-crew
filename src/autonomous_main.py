b616519075f77e4c23abb80633a813278fdfb2d7
Henry-Ash-Williams <henrywilliams95000@gmail.com>
2023-04-21 16:46:54 +0100

    chore: fixed file structure

--- src/autonomous_main.py
+++ src/autonomous_main.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 
-import json
 from game import Game
-from board import BoardEncoder
+from player import AutonomousPlayer
 
 def main():
     """Creates a game between 4 autonomous players and starts it, for testing"""
-
-    game = Game()
-
-    game.add_autonomous_player("red")
-    game.add_autonomous_player("green")
-    game.add_autonomous_player("blue")
-    game.add_autonomous_player("yellow")
+    game = Game(getter=input,players=[AutonomousPlayer(color) for color in ['red','green','blue','purple']])
 
     game.start()
-
-    with open('end_board.json','w') as f:
-        f.write(json.dumps(game.board, cls=BoardEncoder, indent=4))
 
 
 if __name__ == "__main__":
